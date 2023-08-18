0443a04d012d4773c907f4274d9bf13f9dbb2d6a
Abiria <abiria.dev@gmail.com>
2023-08-18 15:26:08 +0900

    feat: add basic structures

---
+++ src/lib.rs
@@ -0,0 +1,19 @@
+#[non_exhaustive]
+enum Gs1Path {
+	GtinPath {
+		gtin: String,
+		cpv: Option<String>,
+		lot: Option<String>,
+		ser: Option<String>,
+	},
+}
+
+#[non_exhaustive]
+struct DataAttributes {
+	net_weight_vmti: String,
+}
+
+struct DigitalLink {
+	gs1_path: Gs1Path,
+	data_attributes: DataAttributes,
+}
