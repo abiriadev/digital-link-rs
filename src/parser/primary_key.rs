cc4d8db2c579c0c4dc8afe86c8bc0dbdf0f499db
Abiria <abiria.dev@gmail.com>
2023-08-21 15:22:18 +0900

    fix: use `ychar` instead of `xchar`

--- src/parser/primary_key.rs
+++ src/parser/primary_key.rs
@@ -4,12 +4,12 @@ use nom::{
 	sequence::pair,
 };
 
-use super::primitive::{digit, xchar};
+use super::primitive::{digit, xchar, ychar};
 
 comp! { gtin,	"01",	count(digit, 14) }
 comp! { itip,	"8006",	count(digit, 18) }
 comp! { gmn,	"8013",	many_m_n(1, 25, xchar) }
-comp! { cpid,	"8010",	many_m_n(1, 30, xchar) }
+comp! { cpid,	"8010",	many_m_n(1, 30, ychar) }
 comp! { gln,	"414",	count(digit, 13) }
 comp! { pay_to,	"415",	count(digit, 13) }
 comp! { party_gln,	"417",	count(digit, 13) }
