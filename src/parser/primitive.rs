beaf3522cbdd613314f7996f2701afa8262a372e
Abiria <abiria.dev@gmail.com>
2023-08-21 10:45:45 +0900

    refactor: make unit test looks cleaner

--- src/parser/primitive.rs
+++ src/parser/primitive.rs
@@ -1,9 +1,4 @@
-use insta::{assert_debug_snapshot, assert_yaml_snapshot};
-use nom::{
-	character::complete::one_of,
-	error::{Error, ErrorKind, ParseError},
-	Err, IResult,
-};
+use nom::{character::complete::one_of, IResult};
 
 fn xchar(i: &str) -> IResult<&str, char> {
 	one_of(
@@ -11,17 +6,24 @@ fn xchar(i: &str) -> IResult<&str, char> {
 	)(i)
 }
 
-#[test]
-fn colon_should_be_xsymbol() {
-	assert_eq!(xchar(":"), Ok(("", ':')));
-}
+#[cfg(test)]
+mod tests {
+	use insta::assert_debug_snapshot;
 
-#[test]
-fn x_should_be_xsymbol() {
-	assert_eq!(xchar("x"), Ok(("", 'x')));
-}
+	use super::*;
 
-#[test]
-fn hash_should_not_be_xsymbol() {
-	assert_debug_snapshot!(xchar("#"));
+	#[test]
+	fn colon_should_be_xsymbol() {
+		assert_eq!(xchar(":"), Ok(("", ':')));
+	}
+
+	#[test]
+	fn x_should_be_xsymbol() {
+		assert_eq!(xchar("x"), Ok(("", 'x')));
+	}
+
+	#[test]
+	fn hash_should_not_be_xsymbol() {
+		assert_debug_snapshot!(xchar("#"));
+	}
 }
