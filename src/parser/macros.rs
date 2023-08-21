7f3bcd2d68a103abf7e21ad4b5db8f54aa95be61
Abiria <abiria.dev@gmail.com>
2023-08-21 11:51:11 +0900

    fix: add public accessor

--- src/parser/macros.rs
+++ src/parser/macros.rs
@@ -1,6 +1,6 @@
 macro_rules! comp {
 	($name:ident, $code:literal, $value:expr) => {
-		fn $name(i: &str) -> nom::IResult<&str, &str> {
+		pub fn $name(i: &str) -> nom::IResult<&str, &str> {
 			nom::combinator::recognize(nom::sequence::pair(
 				nom::sequence::preceded(
 					nom::bytes::complete::tag("/"),
