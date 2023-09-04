21a3a9f16514ae0bc2a621969d9555c73460e888
Abiria <abiria.dev@gmail.com>
2023-09-04 16:19:14 +0900

    style: align primitives and qualifiers

--- src/parser/primary_key.rs
+++ src/parser/primary_key.rs
@@ -5,19 +5,19 @@ use nom::{
 
 use super::primitive::{digit, xchar, ychar};
 
-comp! { gtin,	"01",	count(digit, 14) }
-comp! { itip,	"8006",	count(digit, 18) }
-comp! { gmn,	"8013",	many_m_n(1, 25, xchar) }
-comp! { cpid,	"8010",	many_m_n(1, 30, ychar) }
-comp! { gln,	"414",	count(digit, 13) }
-comp! { pay_to,	"415",	count(digit, 13) }
+comp! { gtin,		"01",	count(digit, 14) }
+comp! { itip,		"8006",	count(digit, 18) }
+comp! { gmn,		"8013",	many_m_n(1, 25, xchar) }
+comp! { cpid,		"8010",	many_m_n(1, 30, ychar) }
+comp! { gln,		"414",	count(digit, 13) }
+comp! { pay_to,		"415",	count(digit, 13) }
 comp! { party_gln,	"417",	count(digit, 13) }
-comp! { gsrnp,	"8017",	count(digit, 18) }
-comp! { gsrn,	"8018",	count(digit, 18) }
-comp! { gcn,	"255",	many_m_n(13, 25, digit) }
-comp! { sscc,	"00",	count(digit, 18) }
-comp! { gdti,	"253",	pair(count(digit, 13), many_m_n(1, 17, xchar)) }
-comp! { ginc,	"401",	many_m_n(1, 30, xchar) }
-comp! { gsin,	"402",	count(digit, 17) }
-comp! { grai,	"8003",	many_m_n(14, 30, xchar) }
-comp! { giai,	"8004",	many_m_n(1, 30, xchar) }
+comp! { gsrnp,		"8017",	count(digit, 18) }
+comp! { gsrn,		"8018",	count(digit, 18) }
+comp! { gcn,		"255",	many_m_n(13, 25, digit) }
+comp! { sscc,		"00",	count(digit, 18) }
+comp! { gdti,		"253",	pair(count(digit, 13), many_m_n(1, 17, xchar)) }
+comp! { ginc,		"401",	many_m_n(1, 30, xchar) }
+comp! { gsin,		"402",	count(digit, 17) }
+comp! { grai,		"8003",	many_m_n(14, 30, xchar) }
+comp! { giai,		"8004",	many_m_n(1, 30, xchar) }
--- src/parser/qualifier.rs
+++ src/parser/qualifier.rs
@@ -5,12 +5,12 @@ use nom::{
 
 use super::primitive::{digit, xchar};
 
-comp! { cpv,    "22",   many_m_n(1, 20, xchar) }
-comp! { lot,    "10",   many_m_n(1, 20, xchar) }
-comp! { ser,    "21",   many_m_n(1, 20, xchar) }
-comp! { cpsn,   "8011", many_m_n(1, 12, digit) }
-comp! { glnx,   "254",  many_m_n(1, 20, xchar) }
-comp! { refno,  "8020", many_m_n(1, 25, xchar) }
-comp! { srin,   "8019", many_m_n(1, 10, digit) }
-comp! { tpx,    "235",  many_m_n(1, 28, xchar) }
-comp! { uic_ext,    "7040", pair(digit, count(xchar, 3)) }
+comp! { cpv,		"22",	many_m_n(1, 20, xchar) }
+comp! { lot,		"10",	many_m_n(1, 20, xchar) }
+comp! { ser,		"21",	many_m_n(1, 20, xchar) }
+comp! { cpsn,		"8011",	many_m_n(1, 12, digit) }
+comp! { glnx,		"254",	many_m_n(1, 20, xchar) }
+comp! { refno,		"8020",	many_m_n(1, 25, xchar) }
+comp! { srin,		"8019",	many_m_n(1, 10, digit) }
+comp! { tpx,		"235",	many_m_n(1, 28, xchar) }
+comp! { uic_ext,	"7040",	pair(digit, count(xchar, 3)) }
