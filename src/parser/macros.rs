57575ee2cf1ffdf8e3aa3797a0a1ca2bc5b5d8ff
Abiria <abiria.dev@gmail.com>
2023-09-04 16:25:30 +0900

    feat: make independent value recognizer

--- src/parser/macros.rs
+++ src/parser/macros.rs
@@ -18,15 +18,21 @@ macro_rules! xchar {
 
 macro_rules! comp {
 	($name:ident, $code:literal, $value:expr) => {
-		pub fn $name(i: &str) -> nom::IResult<&str, &str> {
-			nom::sequence::preceded(
-				nom::sequence::tuple((
-					nom::bytes::complete::tag("/"),
-					nom::bytes::complete::tag($code),
-					nom::bytes::complete::tag("/"),
-				)),
-				nom::combinator::recognize($value),
-			)(i)
+		paste::paste! {
+			pub fn [<$name _value>](i: &str) -> nom::IResult<&str, &str> {
+				nom::combinator::recognize($value)(i)
+			}
+
+			pub fn $name(i: &str) -> nom::IResult<&str, &str> {
+				nom::sequence::preceded(
+					nom::sequence::tuple((
+						nom::bytes::complete::tag("/"),
+						nom::bytes::complete::tag($code),
+						nom::bytes::complete::tag("/"),
+					)),
+					[<$name _value>],
+				)(i)
+			}
 		}
 	};
 }
