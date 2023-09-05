45d021e6c9469faa262e0c68fd8d3ceae9387849
Abiria <abiria.dev@gmail.com>
2023-09-05 13:59:18 +0900

    docs: add example code snippet

---
+++ examples/parse.rs
@@ -0,0 +1,13 @@
+use digital_link_rs::{self, DigitalLink, Gs1Path};
+
+fn main() {
+	let digital_link = DigitalLink::try_from_str("https://id.gs1.org/00/195201234567891232?02=09520123456788&37=25&10=ABC12&17=180426").expect("parsing error");
+
+	if let Gs1Path::Sscc(sscc) = digital_link.gs1_path {
+		println!("sscc: {sscc}");
+	}
+
+	if let Some(date) = digital_link.data_attributes.expiry_date {
+		println!("expiry date: {date}");
+	}
+}
